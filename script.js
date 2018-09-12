4cc986e7340f130057b4739ee9fcb5f5383dd653
jegramin <jegroamin@gmail.com>
2018-09-12 17:01:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("i").addEventListener("click", function(){
+    document.getElementById("bar").classList.toggle("topBar");
+    document.getElementById("sideBar").classList.toggle("shown");
+    document.getElementById("animatedI").classList.toggle("animatedI");
+}) 
